242223d46a13659f6f09646f9ed8d688bccccee5
ForwardGT <102685124+ForwardGT@users.noreply.github.com>
2023-04-25 22:46:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+const increment = () => {
+  count.value ++
+}
+
+</script>
+
+<template>
+  <div>
+    <span>
+      <button @click="increment"> Count
+          {{ count }}
+      </button>
+    </span>
+  </div>
+</template>
