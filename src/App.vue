5c1ead6b94ab7274793d306d2550ce0a2f636091
ForwardGT <102685124+ForwardGT@users.noreply.github.com>
2023-05-22 02:28:43 +0300

    comment string :D

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Navbar from "./components/Navbar.vue"
-import Carousel from "./components/Carousel.vue"
+// import Carousel from "./components/Carousel.vue"
 import AboutAsHomePage from "./components/AboutAsHomePage.vue"
 </script>
 
